8e92b9c99058b21c5860a94a76a3aefdb164e56f
fanhongxuan <fanhongxuan@gmail.com>
2019-08-14 10:26:56 +0800

    1) Disable Logi output, keep Loge output.

--- liveMedia/ByteStreamFileSource.cpp
+++ liveMedia/ByteStreamFileSource.cpp
@@ -25,12 +25,6 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 ////////// ByteStreamFileSource //////////
 #include "LocalSdk.h"
 #define READ_FROM_FILES_SYNCHRONOUSLY
-void logBegin(const char *type, const char *file, const char *function, int line);
-void logEnd();
-
-// #define Logi(...) do{logBegin("info" , __FILE__, __FUNCTION__, __LINE__);printf(__VA_ARGS__); logEnd();}while(0)
-#define Logi(...)
-#define Loge(...) do{logBegin("error", __FILE__, __FUNCTION__, __LINE__);printf(__VA_ARGS__); logEnd();}while(0)
 
 class H264FrameBuffer{
 public:
